fbe914c7f196bceef4dc3ba194b0abcdc908b92c
MinhNhat1505 <nhatthptbhh@gmail.com>
2023-08-18 00:11:50 +0700

    update ball game

---
+++ Chuong5/BallGame.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Chuong5
+{
+    partial class BallGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picBall = new System.Windows.Forms.PictureBox();
+            this.lbCrossBar = new System.Windows.Forms.Label();
+            this.timerBallMove = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBall
+            // 
+            this.picBall.Location = new System.Drawing.Point(316, 30);
+            this.picBall.Name = "picBall";
+            this.picBall.Size = new System.Drawing.Size(121, 87);
+            this.picBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBall.TabIndex = 0;
+            this.picBall.TabStop = false;
+            // 
+            // lbCrossBar
+            // 
+            this.lbCrossBar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbCrossBar.Location = new System.Drawing.Point(299, 389);
+            this.lbCrossBar.Name = "lbCrossBar";
+            this.lbCrossBar.Size = new System.Drawing.Size(184, 27);
+            this.lbCrossBar.TabIndex = 1;
+            this.lbCrossBar.MouseDown += new System.Windows.Forms.MouseEventHandler(this.lbCrossBar_MouseDown);
+            this.lbCrossBar.MouseMove += new System.Windows.Forms.MouseEventHandler(this.lbCrossBar_MouseMove);
+            // 
+            // timerBallMove
+            // 
+            this.timerBallMove.Interval = 30;
+            this.timerBallMove.Tick += new System.EventHandler(this.timerBallMove_Tick);
+            // 
+            // BallGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(857, 475);
+            this.Controls.Add(this.lbCrossBar);
+            this.Controls.Add(this.picBall);
+            this.Name = "BallGame";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Ball Game";
+            this.Load += new System.EventHandler(this.BallGame_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.BallGame_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.picBall)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBall;
+        private System.Windows.Forms.Label lbCrossBar;
+        private System.Windows.Forms.Timer timerBallMove;
+    }
+}
+
---
+++ Chuong5/BallGame.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chuong5
+{
+    public partial class BallGame : Form
+    {
+        //khoảng cách mỗi lần ball di chuyển
+        private int moveStepX = 5;
+        private int moveStepY = 8;
+
+        //lưu điểm mặc định của ball và cross bar
+        private Point pDefaultBall, pDefaultCrossBar;
+
+        //khoảng cách mỗi lần cross bar di chuyển
+        private int crossBarMove = 20;
+
+        //lưu vị trí con trỏ chuột khi click vào cross bar
+        private Point pCursorCB;
+        public BallGame()
+        {
+            InitializeComponent();
+        }
+
+        private void RestartTheGame()
+        {
+            picBall.Location = pDefaultBall;
+            lbCrossBar.Location = pDefaultCrossBar;
+        }
+
+        private void BallGame_Load(object sender, EventArgs e)
+        {
+            picBall.Image = Image.FromFile(Application.StartupPath + @"\images\ball.png");
+            pDefaultBall = picBall.Location;
+            pDefaultCrossBar = lbCrossBar.Location;
+        }
+
+        private void BallGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch(e.KeyCode) 
+            {
+                case Keys.S:
+                    timerBallMove.Start();
+                    break;
+                case Keys.Left:
+                    if (lbCrossBar.Left - crossBarMove >= 0)
+                        lbCrossBar.Left -= crossBarMove;
+                    break;
+                case Keys.Right:
+                    if(lbCrossBar.Right + crossBarMove <= ClientRectangle.Width)
+                        lbCrossBar.Left += crossBarMove;
+                    break;
+            }
+        }
+
+        private void lbCrossBar_MouseDown(object sender, MouseEventArgs e)
+        {
+            //lấy vị trí con trỏ chuột khi click vào cross bar
+            pCursorCB = e.Location;
+        }
+
+        private void lbCrossBar_MouseMove(object sender, MouseEventArgs e)
+        {
+            //tính xem con trỏ chuột đã di chuyển bao xa so với vị trí ban đầu
+            int dx = e.X - pCursorCB.X;
+
+            if(e.Button == MouseButtons.Left)
+            {
+                if(lbCrossBar.Left + dx > 0 && lbCrossBar.Right + dx <  ClientRectangle.Width)
+                    lbCrossBar.Left += dx;
+                
+            }
+        }
+
+        private void timerBallMove_Tick(object sender, EventArgs e)
+        {
+            //nếu ball chạm góc trái màn hình hoặc góc phải => cho đi ngược chiều trục hoành
+            if (picBall.Left <= 0 || picBall.Right > ClientRectangle.Width)
+                moveStepX = -moveStepX;
+
+            //nếu ball đụng phía trên cùng hoặc đụng thanh scross bar thì cho đi ngược chiều trục tung
+            if(picBall.Top <= 0 || picBall.Bounds.IntersectsWith(lbCrossBar.Bounds))
+                moveStepY = -moveStepY;
+
+            //cho quả bóng di chuyển
+            picBall.Left += moveStepX;
+            picBall.Top += moveStepY;
+
+            //nếu Y của ball > Y của cross ball thì cho dừng lại
+            if (picBall.Bottom > lbCrossBar.Bottom)
+            {
+                timerBallMove.Stop();
+
+                if (MessageBox.Show("Bạn thua rồi,muốn chơi lại không?", "Xác nhận"
+                    , MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    RestartTheGame();
+                }
+                else
+                    Close();
+
+            }
+
+
+        }
+    }
+}
