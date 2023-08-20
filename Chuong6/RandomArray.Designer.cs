1a489de13c6687fce58f5d8183e6e92fa9d841a8
MinhNhat1505 <nhatthptbhh@gmail.com>
2023-08-20 18:25:41 +0700

    add random array

---
+++ Chuong6/RandomArray.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Chuong6
+{
+    partial class RandomArray
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtArrayElement = new System.Windows.Forms.TextBox();
+            this.txtSumEvenElements = new System.Windows.Forms.TextBox();
+            this.txtSumOddElements = new System.Windows.Forms.TextBox();
+            this.txtAverage = new System.Windows.Forms.TextBox();
+            this.txtPrimeArray = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDefaultArray = new System.Windows.Forms.TextBox();
+            this.btnArrayGeneration = new System.Windows.Forms.Button();
+            this.btnStatistic = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Số phần tử:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(261, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tổng các phần tử chẳn:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(261, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tổng các phần tử lẻ:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 321);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(326, 34);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Trung bình cộng các phần tử:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 405);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(290, 34);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Số các số nguyên tố:";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtArrayElement
+            // 
+            this.txtArrayElement.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtArrayElement.Location = new System.Drawing.Point(173, 24);
+            this.txtArrayElement.Name = "txtArrayElement";
+            this.txtArrayElement.Size = new System.Drawing.Size(50, 30);
+            this.txtArrayElement.TabIndex = 5;
+            // 
+            // txtSumEvenElements
+            // 
+            this.txtSumEvenElements.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSumEvenElements.Location = new System.Drawing.Point(310, 179);
+            this.txtSumEvenElements.Name = "txtSumEvenElements";
+            this.txtSumEvenElements.ReadOnly = true;
+            this.txtSumEvenElements.Size = new System.Drawing.Size(63, 30);
+            this.txtSumEvenElements.TabIndex = 6;
+            // 
+            // txtSumOddElements
+            // 
+            this.txtSumOddElements.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSumOddElements.Location = new System.Drawing.Point(310, 254);
+            this.txtSumOddElements.Name = "txtSumOddElements";
+            this.txtSumOddElements.ReadOnly = true;
+            this.txtSumOddElements.Size = new System.Drawing.Size(63, 30);
+            this.txtSumOddElements.TabIndex = 7;
+            // 
+            // txtAverage
+            // 
+            this.txtAverage.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAverage.Location = new System.Drawing.Point(344, 325);
+            this.txtAverage.Name = "txtAverage";
+            this.txtAverage.ReadOnly = true;
+            this.txtAverage.Size = new System.Drawing.Size(79, 30);
+            this.txtAverage.TabIndex = 8;
+            // 
+            // txtPrimeArray
+            // 
+            this.txtPrimeArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrimeArray.Location = new System.Drawing.Point(310, 405);
+            this.txtPrimeArray.Name = "txtPrimeArray";
+            this.txtPrimeArray.ReadOnly = true;
+            this.txtPrimeArray.Size = new System.Drawing.Size(455, 30);
+            this.txtPrimeArray.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(290, 34);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Mảng ban đầu:";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtDefaultArray
+            // 
+            this.txtDefaultArray.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDefaultArray.Location = new System.Drawing.Point(308, 104);
+            this.txtDefaultArray.Name = "txtDefaultArray";
+            this.txtDefaultArray.ReadOnly = true;
+            this.txtDefaultArray.Size = new System.Drawing.Size(455, 30);
+            this.txtDefaultArray.TabIndex = 11;
+            // 
+            // btnArrayGeneration
+            // 
+            this.btnArrayGeneration.Location = new System.Drawing.Point(293, 21);
+            this.btnArrayGeneration.Name = "btnArrayGeneration";
+            this.btnArrayGeneration.Size = new System.Drawing.Size(187, 34);
+            this.btnArrayGeneration.TabIndex = 12;
+            this.btnArrayGeneration.Text = "Sinh mãng ngẫu nhiên";
+            this.btnArrayGeneration.UseVisualStyleBackColor = true;
+            this.btnArrayGeneration.Click += new System.EventHandler(this.btnArrayGeneration_Click);
+            // 
+            // btnStatistic
+            // 
+            this.btnStatistic.Location = new System.Drawing.Point(539, 20);
+            this.btnStatistic.Name = "btnStatistic";
+            this.btnStatistic.Size = new System.Drawing.Size(187, 34);
+            this.btnStatistic.TabIndex = 13;
+            this.btnStatistic.Text = "Thống kê";
+            this.btnStatistic.UseVisualStyleBackColor = true;
+            this.btnStatistic.Click += new System.EventHandler(this.btnStatistic_Click);
+            // 
+            // RandomArray
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStatistic);
+            this.Controls.Add(this.btnArrayGeneration);
+            this.Controls.Add(this.txtDefaultArray);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtPrimeArray);
+            this.Controls.Add(this.txtAverage);
+            this.Controls.Add(this.txtSumOddElements);
+            this.Controls.Add(this.txtSumEvenElements);
+            this.Controls.Add(this.txtArrayElement);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RandomArray";
+            this.Text = "RandomArray";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.RandomArray_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtArrayElement;
+        private System.Windows.Forms.TextBox txtSumEvenElements;
+        private System.Windows.Forms.TextBox txtSumOddElements;
+        private System.Windows.Forms.TextBox txtAverage;
+        private System.Windows.Forms.TextBox txtPrimeArray;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDefaultArray;
+        private System.Windows.Forms.Button btnArrayGeneration;
+        private System.Windows.Forms.Button btnStatistic;
+    }
+}
+
---
+++ Chuong6/RandomArray.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chuong6
+{
+    public partial class RandomArray : Form
+    {
+        //chứa mảng ban đầu
+        private List<int> defaultArray = new List<int>();
+
+        private Random rd = new Random();
+        public RandomArray()
+        {
+            InitializeComponent();
+        }
+
+        private bool isPrime(int num)
+        {
+            if(num < 2) return false;
+
+            if (num == 2) return true;
+
+            for(int i = 2;i <= Math.Sqrt(num);i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+        private void btnArrayGeneration_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                txtDefaultArray.Text = "";
+                if(defaultArray != null ) { defaultArray.Clear(); }
+
+                int n = int.Parse(txtArrayElement.Text);
+                for(int i = 0; i < n;i++) 
+                {
+                    defaultArray.Add(rd.Next(100));
+                }
+
+                //gán lại vào textbox
+                txtDefaultArray.Text = String.Join(", ", defaultArray.Select(num => num.ToString()));
+            }
+            catch(FormatException)
+            {
+                MessageBox.Show("Vui lòng nhập số lượng phần tử");
+            }
+        }
+
+        private void btnStatistic_Click(object sender, EventArgs e)
+        {
+            int sumEven = 0;
+            int sumOdd = 0;
+            int total = 0;
+            List<int> primeNumber = new List<int>();
+
+            foreach(int i in defaultArray) 
+            {
+                //cộng chẳn lẻ
+                if(i % 2 == 0)
+                    sumEven += i;
+                else
+                    sumOdd += i;
+
+                //liệt kê số nguyên tố
+                if(isPrime(i))
+                    primeNumber.Add(i);
+
+                //cộng tổng các số trong mảng
+                total += i;
+            }
+
+            //gán giá trị vào textbox
+            txtSumEvenElements.Text = sumEven.ToString();
+            txtSumOddElements.Text = sumOdd.ToString();
+            txtAverage.Text = String.Format("{0:0.000}",total * 1.0 / defaultArray.Count);
+            txtPrimeArray.Text = primeNumber.Count == 0 ? "Không có số nguyên tố" : String.Join(", ", primeNumber.Select(num => num.ToString()));
+
+        }
+
+        private void RandomArray_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Bạn có muốn thoát không?","Cảnh báo",MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.No)
+                e.Cancel = true;
+        }
+    }
+}
